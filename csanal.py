caec2bcb45803c5ca29d72a12530b3540468cb4f
chasdabigone <chasdabigone@gmail.com>
2015-07-09 20:50:05 -0500

    Update csanal.py

--- csanal.py
+++ csanal.py
@@ -2,11 +2,19 @@ import json
 import requests
 csgo_json = requests.get("http://csgo.hvalrossen.dk/")
 parsed_json = json.loads(csgo_json.text)
-"""%matplotlib inline
+%matplotlib inline
 import matplotlib.pyplot as plt
-import pandas as pd #this is how I usually import pandas
+import pandas as pd
 from pandas.io.json import json_normalize
-normalized_json = json_normalize(parsed_json['matches'])""" #will use this later
+normalized_json = json_normalize(parsed_json['matches'])
+
+"""def teamonmap(team,maps)
+    
+    matches = []
+    for m in parsed_json['matches']:
+        if m['team_a'] == team or m['team_b'] == team and m[]
+            matches.append(m)""" #no map data for this yet
+
 def tvt(team1,team2):
     
     matches = []
@@ -18,10 +26,10 @@ def tvt(team1,team2):
     team1h2hpercent = []
     team2h2hpercent = []
     for m in parsed_json['matches']:
-        if m['team_a'] == team1 or m['team_b'] == team1 or m['team_a'] == team2 or m['team_b'] == team2:
+        if (m['team_a'] == team1 or m['team_b'] == team1) or (m['team_a'] == team2 or m['team_b'] == team2):
             matches.append(m)
     for h in parsed_json['matches']:
-        if h['team_a'] == team1 and h['team_b'] == team2 or h['team_a'] == team2 and h['team_b'] == team1:
+        if (h['team_a'] == team1 and h['team_b'] == team2) or (h['team_a'] == team2 and h['team_b'] == team1):
             h2hmatches.append(h)
     team1wins = [x for x in matches if x['winner'] == team1]
     team2wins = [x for x in matches if x['winner'] == team2]
@@ -29,11 +37,14 @@ def tvt(team1,team2):
     team2h2h = [x for x in h2hmatches if x['winner'] == team2]
     team1h2hpercent = round(float(len(team1h2h)) / float(len(h2hmatches)),3)
     team2h2hpercent = round(float(len(team2h2h)) / float(len(h2hmatches)),3)
+    impliedoddsteam1 = round(team1h2hpercent / (team1h2hpercent + team2h2hpercent), 2)
+    impliedoddsteam2 = round(team2h2hpercent / (team1h2hpercent+team2h2hpercent),2)
     print '{0} {1}{2}{3}{4}: {5}'.format("Total",team1," vs ",team2," matches",len(h2hmatches))
     print '{0} {1}: {2}'.format(team1,"head to head wins",len(team1h2h))
     print '{0} {1}: {2}{3}'.format(team1,"head to head win percentage",team1h2hpercent*100, "%")
     print '{0} {1}: {2}'.format(team2,"head to head wins",len(team2h2h))
     print '{0} {1}: {2}{3}'.format(team2,"head to head win percentage",team2h2hpercent*100, "%")
+    print '{0}: {1} {2}{3} : {4}{5} {6}'.format("Implied odds",team1,impliedoddsteam1*100,"%",impliedoddsteam2*100,"%",team2)
         
 def team_win_percentage(team):
     
